90ddf4bd91aad64accc6f7390911652255ee4d2a
Nataly Kozyreva <eve30@yandex.ru>
2023-07-03 23:07:16 +0300

    add form functions

--- form/assets/script/script.js
+++ form/assets/script/script.js
@@ -1,65 +1,129 @@
-
-// function ValidatePassword(e) {
-//     e.preventDefault();
-//     const passwordField = form.elements.password;
-//     const passwordFormat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,20}$/;
-//     if (passwordField.value.match(passwordFormat)) {
-//         console.log("Ваш адрес электронной почты введён верно!")
-//         return true;
-//     } else {
-//         console.log("Ваш адрес электронной почты введён неверно!");
-//         return false;
-//     }
-// }
-
-//предотвращаем отправку поля до правильного заполнения
-function handleFormSubmit(event) {
-event.preventDefault();
-console.log('sent');
-}
-
 const form = document.forms.form;
-form.addEventListener('submit', handleFormSubmit);
 
-function checkValidity(input) {
-let validity = input.validity;
-let errorElement = document.createElement('span');
-if (validity.valueMissing) {
-    let div = document.getElementById('name');
-    input.classList.add('error');
-    errorElement.textContent = 'Поле ' + input.placeholder + ' не заполнено';
-    div.append(errorElement);
-}
-// if (validity.patternMismatch) {
-    // let div2 = document.getElementById('email');
-//     input.classList.add('error');
-//     errorElement.textContent = 'Неверный формат заполнения';
-//     div2.append(errorElement);
-// }
-if (validity.rangeOverflow) {
-    let div3 = document.getElementById('age');
-    input.classList.add('error');
-    errorElement.textContent = 'Значение превосходит максимально допустимое';
-    div3.append(errorElement);
-}
-if (validity.rangeUnderflow) {
-    let div4 = document.getElementById('age');
-    input.classList.add('error');
-    errorElement.textContent = 'Значение меньше минимально допустимого';
-    div4.append(errorElement);
+const userName = form.elements.name;
+function checkName(userName) {
+    const rightName = userName.value.trim();
+    const divName = document.getElementById('name');
+    if (rightName === "") {
+        const error = document.createElement('span');
+        userName.classList.add('error');
+        error.textContent = 'Введите имя'
+        divName.append(error);
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const userEmail = form.elements.email;
+function checkEmail(userEmail) {
+    const rightEmail = userEmail.value;
+    const divEmail = document.getElementById('email');
+    const mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    if (rightEmail.match(mailFormat)) {
+        return true;
+    } else if (rightEmail.value === '') {
+        const error = document.createElement('span');
+        userEmail.classList.add('error');
+        error.textContent = 'Введите email'
+        divEmail.append(error);
+        return false;
+    } else {
+        const error = document.createElement('span');
+        userEmail.classList.add('error');
+        error.textContent = 'Введите корректный email'
+        divEmail.append(error);
+        return false;
+    }
+};
+
+
+const userAge = form.elements.age;
+function checkAge(userAge) {
+    const divAge = document.getElementById('age');
+    if (userAge.value === "") {
+        const error = document.createElement('span');
+        userAge.classList.add('error');
+        error.textContent = 'Введите возраст'
+        divAge.append(error);
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const password = form.elements.password;
+function checkPassword(password) {
+    const divPassword = document.getElementById('password');
+    const rightPassword = password.value;
+    const correctPassword = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+    if (rightPassword.match(correctPassword)) {
+        return true;
+    } else {
+        const error = document.createElement('span');
+        password.classList.add('error');
+        error.textContent = 'Введите корректный пароль'
+        divPassword.append(error);
+        return false;
+    }
+};
+
+const repeatPassword = form.elements.repeatpassword;
+function checkPasswords(password, repeatpassword) {
+    const divCheckPassword = document.getElementById('password-check');
+    if (password.value === repeatpassword.value) {
+        return true;
+    } else {
+        const error = document.createElement('span');
+        repeatPassword.classList.add('error');
+        error.textContent = 'Пароли не совпадают';
+        divCheckPassword.append(error);
+        return false;
+    }
+};
+
+const checkbox = form.elements.checkbox;
+function checkAgreement(checkbox) {
+    const divCheckbox = document.getElementById('checkbox');
+    if (checkbox.checked) {
+        return true;
+    } else {
+        const error = document.createElement('span');
+        checkbox.classList.add('error');
+        error.textContent = 'Ознакомьтесь с условиями';
+        divCheckbox.append(error);
+        return false;
+    }
+};
+
+function checkValidity() {
+    if (checkName(userName) === false ||
+        checkAge(userAge) === false ||
+        checkEmail(userEmail) === false ||
+        checkPassword(password) === false ||
+        checkPasswords(password, repeatPassword) === false ||
+        checkAgreement(checkbox) === false) {
+        document.getElementById('errorsInfo').textContent = 'Заполните все поля';
+        return false;
+    } else {
+        form.reset();
+        return true;
     }
 }
 
-//Проверка для всех полей
-function checkAll() {
-		//Получаем все инпуты
-    let inputs = document.querySelectorAll("input");
-		//Перебираем их и на каждый вызываем функцию валидации
-    for (let input of inputs) {
-        checkValidity(input);
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    checkName(userName);
+    checkEmail(userEmail);
+    checkAge(userAge);
+    checkPassword(password);
+    checkPasswords(password, repeatPassword);
+    checkAgreement(checkbox);
+    if (checkValidity() === true) {
+        const div = document.getElementById('errorsInfo');
+        const text = document.createElement('p');
+        div.textContent = 'Регистрация завершена';
+        div.append(text);
     }
+});
 
-}
-
-const btn = form.elements.button;
-btn.addEventListener('click', checkAll);
